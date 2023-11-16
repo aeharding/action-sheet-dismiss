e99fb0f9b35b44ab5e690815001b70bfa3654619
Alexander Harding <2166114+aeharding@users.noreply.github.com>
2023-11-15 19:34:27 -0600

    Add repro

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,12 +1,16 @@
-import './ExploreContainer.css';
+import { IonButton, useIonActionSheet } from "@ionic/react";
+import "./ExploreContainer.css";
 
-interface ContainerProps { }
+interface ContainerProps {}
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
+  const [presentActionSheet] = useIonActionSheet();
+  function present() {
+    presentActionSheet([{ text: "Foo" }, { text: "Baz" }]);
+  }
   return (
     <div id="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <IonButton onClick={present}>Present action sheet</IonButton>
     </div>
   );
 };
